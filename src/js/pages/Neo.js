14267abe3ff7ed4b14b36290a5b4a6f8e2fcd3af
FMurry <frederic.wesley.murry@gmail.com>
2017-04-20 23:37:18 -0700

    Fixes to restrictions on API calls

--- src/js/pages/Neo.js
+++ src/js/pages/Neo.js
@@ -43,12 +43,9 @@ export default class Neo extends React.Component{
       startDate,
       endDate
     });
-    console.log("OnSelect Event");       
-    console.log(this.state);
    }
 
    verifyDate() {
-    console.log("Verifying date....");
     if(!(this.state.startRaw) || !(this.state.endRaw)){
       //Dates not selected
       console.log("Date not verified");
@@ -60,18 +57,16 @@ export default class Neo extends React.Component{
     }
     var timeDifference = Math.abs(this.state.endRaw.getTime() - this.state.startRaw.getTime());
     var numOfDays = Math.ceil(timeDifference / (1000 * 3600 * 24)); 
-    if(numOfDays < 7){
-      console.log("Choose greater than 7 days");
+    if(numOfDays < 7 || numOfDays > 7){
+      console.log("7 days only");
     }
     else{
       //Verify here
-      console.log("Date verified");
       this.setState({
         text: "Searching......"
       })
       this.getNeos();
     }
-    console.log(this.state);
    }
    getNeos() {
     console.log("Display Neo event");
@@ -83,29 +78,38 @@ export default class Neo extends React.Component{
     //https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY
     axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${this.state.startDate}&end_date=${this.state.endDate}&api_key=${API_KEY}`)
       .then(res => {
+        console.log(res);
         for(var day in res.data.near_earth_objects){
           for(var neo in res.data.near_earth_objects[day]){
-            console.log(day.toString());
             var name = React.createElement('td',null,res.data.near_earth_objects[day][neo].name);
             var hazardous = React.createElement('td',null,res.data.near_earth_objects[day][neo].is_potentially_hazardous_asteroid.toString());
-            content.push(React.createElement("tr",null,[day,name, hazardous]));
+            var minDiameter = React.createElement('td',null,res.data.near_earth_objects[day][neo].estimated_diameter.meters.estimated_diameter_min);
+            var maxDiameter = React.createElement('td',null,res.data.near_earth_objects[day][neo].estimated_diameter.meters.estimated_diameter_max);
+            var relativeVelocity = React.createElement('td',null, res.data.near_earth_objects[day][neo].close_approach_data[0].relative_velocity.kilometers_per_hour);
+            content.push(React.createElement("tr",null,[day,name, hazardous,minDiameter,maxDiameter,relativeVelocity]));
           }
         }
         var tableHeaders = React.createElement("thead",null,
           React.createElement("tr",null,[
             React.createElement("th",null,"Date"),
             React.createElement("th",null,"Name"), 
-            React.createElement("th",null,"Hazardous?")]));
+            React.createElement("th",null,"Hazardous?"),
+            React.createElement("th",null,"Minimum Diameter (m)"),
+            React.createElement("th",null,"Maximum Diameter (m)"),
+            React.createElement("th",null,"Relative Velocity (km/h")]));
         var tableBody = React.createElement("tbody",null,content);
         var table = React.createElement(Table,{"responsive":true, "striped":true, "bordered":true, "condensed":true, "hover":true},[tableHeaders, tableBody]);
         mySelf.setState({
           text: table
         });
       })
+      .catch( err => {
+        console.log(err);
+      });
    }
 	render() {
     var mySelf = this;
-    var help = "Select up to 7 days in the calendar";
+    var help = "Please select 7 days in the calendar";
 		return(
 			<div>
         <div id='alert'>
